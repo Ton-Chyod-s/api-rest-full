f91d984c8111665c10bf8d7d84c18f6a89dbdd19
Klayton Dias <klayton.dias@nextmed.com.br>
2024-12-21 22:11:03 -0400

    feat: projeto console CrossCutting

--- DiarioOficial.API/obj/Debug/net8.0/DiarioOficial.API.AssemblyInfo.cs
+++ DiarioOficial.API/obj/Debug/net8.0/DiarioOficial.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiarioOficial.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b5cd7738dd0b3947623c100a74030683d7a55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2ab33ac1c0175a0c964dff335ca90efa529267")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiarioOficial.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiarioOficial.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiarioOficial.Application/obj/Debug/net8.0/DiarioOficial.Application.AssemblyInfo.cs
+++ DiarioOficial.Application/obj/Debug/net8.0/DiarioOficial.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiarioOficial.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc22ce8b1ed01c1ad9d4565d81beb44f11719498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2ab33ac1c0175a0c964dff335ca90efa529267")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiarioOficial.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiarioOficial.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
