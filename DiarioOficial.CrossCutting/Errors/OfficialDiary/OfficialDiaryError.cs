7c6725b6de027599b62ff0b16566617558ed1ce5
Klayton Dias <klayton.dias@nextmed.com.br>
2024-12-23 07:02:08 -0400

    feat: nova estruturação

--- DiarioOficial.CrossCutting/Errors/OfficialDiary/OfficialDiaryError.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DiarioOficial.CrossCutting.Errors.OfficialDiary
-{
-    public record OfficialDiaryUnexpectedError()
-        : BaseError("Erro inesperado ao tentar acessar o Diário Oficial!", nameof(OfficialDiaryUnexpectedError), StatusCodes.Status500InternalServerError);
-}
