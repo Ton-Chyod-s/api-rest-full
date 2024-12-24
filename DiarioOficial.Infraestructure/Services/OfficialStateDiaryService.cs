ef5ee6836975278cad885912d8d065e1ece8cd8d
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-12-23 21:11:37 -0400

    feat: queryBody

--- DiarioOficial.Infraestructure/Services/OfficialStateDiaryService.cs
+++ DiarioOficial.Infraestructure/Services/OfficialStateDiaryService.cs
@@ -1,10 +1,35 @@
-﻿using DiarioOficial.CrossCutting.DTOs;
+﻿using System.Text.Json;
+using DiarioOficial.CrossCutting.DTOs;
 using DiarioOficial.Domain.Interface.Services.OfficialStateDiary;
+using RestSharp;
 
 namespace DiarioOficial.Infraestructure.Services
 {
     public class OfficialStateDiaryService() : IOfficialStateDiaryService
     {
 
+
+        private string QueryBody(string of, string to)
+        {
+            var queryObject = new
+            {
+                action = "edicoes_json",
+                palavra = " ",
+                numero = "",
+                de = of, 
+                ate = to, 
+                _0 = 0.5278609866886916,
+                draw = 1,
+                start = 0,
+                length = 10,
+                order = new[]
+                {
+                    new { column = 0, dir = "desc" }
+                },
+                search = new { value = "", regex = false }
+            };
+
+            return JsonSerializer.Serialize(queryObject);
+        }
     }
 }
