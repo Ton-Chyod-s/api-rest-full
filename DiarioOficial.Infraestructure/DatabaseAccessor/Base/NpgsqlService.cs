4c999bb6bbabb5ef856a44633f7a8383f81febbc
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-12-27 22:01:28 -0400

    feat: nova class NpgsqlServices

---
+++ DiarioOficial.Infraestructure/DatabaseAccessor/Base/NpgsqlService.cs
@@ -0,0 +1,28 @@
+﻿using DiarioOficial.Domain.Interface.DatabaseAccessor.Base;
+using Npgsql;
+
+namespace DiarioOficial.Infraestructure.DatabaseAccessor.Base
+{
+    internal class NpgsqlService : INpgsqlService
+    { 
+        private readonly NpgsqlConnection _officialDiaryDbConnection;
+
+        public NpgsqlService(NpgsqlConnection officialDiaryDbConnection)
+        {
+            _officialDiaryDbConnection = officialDiaryDbConnection;
+            OpenOfficialDiaryConnectionIfNotOpen();
+        }
+
+        public async Task<NpgsqlDataReader> ExecuteCommandAndReaderAsync(string query)
+        {
+            using var command = new NpgsqlCommand(query, _officialDiaryDbConnection);
+            return await command.ExecuteReaderAsync();
+        }
+
+        private void OpenOfficialDiaryConnectionIfNotOpen()
+        {
+            if (_officialDiaryDbConnection.State != System.Data.ConnectionState.Open)
+                _officialDiaryDbConnection.Open();
+        }
+    }
+}
--- DiarioOficial.Infraestructure/Extensions/ConfigureServicesExtensions.cs
+++ DiarioOficial.Infraestructure/Extensions/ConfigureServicesExtensions.cs
@@ -1,6 +1,8 @@
-﻿using DiarioOficial.Domain.Interface.Services.OfficialElectronicDiary;
+﻿using DiarioOficial.Domain.Interface.DatabaseAccessor.Base;
+using DiarioOficial.Domain.Interface.Services.OfficialElectronicDiary;
 using DiarioOficial.Domain.Interface.Services.OfficialStateDiary;
 using DiarioOficial.Domain.Interface.Services.SendEmail;
+using DiarioOficial.Infraestructure.DatabaseAccessor.Base;
 using DiarioOficial.Infraestructure.Services.OfficialElectronicDiary;
 using DiarioOficial.Infraestructure.Services.OfficialStateDiary;
 using DiarioOficial.Infraestructure.Services.SendEmail;
@@ -13,9 +15,13 @@ namespace DiarioOficial.Infraestructure.Extensions
     {
         public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            #region [Services]
             services.AddTransient<IOfficialStateDiaryService, OfficialStateDiaryService>();
             services.AddTransient<IOfficialElectronicDiaryService, OfficialElectronicDiaryService>();
             services.AddTransient<ISendEmailService, SendEmailService>();
+            #endregion
+
+            services.AddScoped<INpgsqlService, NpgsqlService>();
 
             return services;
         }
